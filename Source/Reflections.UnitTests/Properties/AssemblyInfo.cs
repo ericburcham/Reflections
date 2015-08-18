b032494ec9ee13ef882e5a329bc56ccc2004a51e
Eric Burcham <eburcham@eprod.com>
2015-08-18 14:09:00 -0500

    Got shared assembly info working the way I want.

--- Source/Reflections.UnitTests/Properties/AssemblyInfo.cs
+++ Source/Reflections.UnitTests/Properties/AssemblyInfo.cs
@@ -22,16 +22,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c93b2d50-7e03-476a-8669-8063a22462a5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
