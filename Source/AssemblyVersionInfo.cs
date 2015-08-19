e2ac26b0ec8f1b43250ebb7c6e517fb3a780addd
Eric Burcham <eburcham@eprod.com>
2015-08-19 15:33:16 -0500

    Updated version info documentation.

--- Source/AssemblyVersionInfo.cs
+++ Source/AssemblyVersionInfo.cs
@@ -2,13 +2,13 @@ using System.Reflection;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Patch Number
-//      Revision
+//  We use Semantic Versioning:  http://semver.org/
+//  Given a version number MAJOR.MINOR.PATCH.REVISION, increment the:
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+//      MAJOR Version - When you make incompatible API changes,
+//      MINOR Version - When you add functionality in a backwards-compatible manner, and
+//      PATCH Version - When you make backwards-compatible bug fixes.
+//      REVISION Version - Never.
+//
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
